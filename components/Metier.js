6b3b2a82fd7e181a62032cd5abceca37cdc6abcb
Toy GB <toygb55@gmail.com>
2022-12-05 10:21:24 +0000

    new update

--- components/Metier.js
+++ components/Metier.js
@@ -1,20 +1,73 @@
 import * as React from "react";
-import { Image, StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View, FlatList } from "react-native";
 import {Foundation} from '@expo/vector-icons'
 import Storylist from "./storylist/storylist";
 import { StatusBar } from "expo-status-bar";
 import Constants from "expo-constants"
+import { Ionicons } from '@expo/vector-icons';
+
+
+const DATA = [
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    title: 'First Item',
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+    title: 'Second Item',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    title: 'Third Item',
+  },
+];
+
+const Item = ({ title }) => (
+  <View style={styles.item}>
+    <Image />
+    <Text style={styles.title}>{title}</Text>
+  </View>
+);
 
 const Metier = () => {
+
+  let metier = "Médecin"
+  const img = require("../assets/med.png")
+
+  const renderItem = ({ item }) => (
+    <Item title={item.title} />
+  );
+
   return (
     <View style={styles.container}>
       <Storylist />
-      <View style={{width:'100%', justifyContent:'center', alignItems:'center', alignSelf:'center', marginTop:'10%'}}>
-        <Image 
-          source={require('../assets/weekjob.png')}
-          style={{width:'95%', height:215, borderRadius:20}}
+      <View style={{justifyContent:'center', alignItems:'center', alignSelf:'center', marginTop:'10%'}}>
+        <View style={{backgroundColor:'#047FEE', width:330, height:155, borderRadius:20, alignItems:'center'}}>
+          {
+            metier == "Médecin" ? 
+          <View style={{justifyContent:'center', flexDirection:'row'}}>
+            <Image source={img} style={{width:70, height:150, zIndex:2, position:'relative', top:-25}} />
+            <View style={{justifyContent:'center', position:'relative', left:'15%'}}>
+              <Text style={{color:'white',fontSize:20, marginBottom:'10%'}}>Métier de la semaine: </Text>
+              <Text style={styles.texteMetier}>{metier}</Text>
+            </View>
+          </View>
+          :
+          null
+          }
+        </View>
+      </View>
+      <View style={{flexDirection:'row', alignItems:'center', justifyContent:'space-between', paddingHorizontal:25, marginTop:'10%'}}>
+        <Text style={{fontWeight:'700'}}>Liste des métiers</Text>
+        <View style={{borderWidth:1, width:150, height:0, borderColor:'#DADADA', backgroundColor:'#DADADA'}} />
+        <Ionicons name="md-search-outline" size={24} color="black" />
+      </View>
+      <View style={{paddingHorizontal:25}}>
+        <FlatList
+          data={DATA}
+          renderItem={renderItem}
+          keyExtractor={item => item.id}
         />
-        <Text style={styles.texteMetier}>Médecin</Text>
       </View>
     </View>
   );
@@ -25,12 +78,18 @@ const styles = StyleSheet.create({
     paddingTop: Constants.statusBarHeight
   },
   texteMetier:{
-    position:'relative',
-    left:'0%',
-    bottom:'35%',
     color:'#fff',
     fontWeight:'700',
-    fontSize:35
+    fontSize:35,
+    textAlign:'center'
+  },
+  item:{
+    flexDirection:'row',
+    backgroundColor:"#DADADA",
+    paddingVertical:'10%',
+    marginVertical:'3%',
+    alignItems:'center',
+    padding:'5%'
   }
 })
 
--- components/storylist/storylist.js
+++ components/storylist/storylist.js
@@ -1,5 +1,6 @@
-import { ScrollView, Pressable, StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native'
-import React from 'react'
+import { ScrollView, Pressable, StyleSheet, Text, View, Image, TouchableOpacity, ActivityIndicatorBase } from 'react-native'
+import React from 'react';
+import InstaStory from 'react-native-insta-story';
 
 const stories = [
     {
@@ -30,27 +31,258 @@ const stories = [
     
 ]
 
-const Storylist = ()=> {
-  return (
-    <View style={styles.container}    
-    >
-        <View style={styles.storyContainer}>
-                {
-                    stories.map((story, index) =>{
-                        return(
-                            <TouchableOpacity>
-                                <View style={styles.circleContainer} >
-                                    <ProfileHead key={story.id} uri={story.uri} />
-                                    <Text>{story.jour}</Text>
-                                </View>
-                            </TouchableOpacity>
-                        );
-                    })
-                }
-        </View>
-    </View>
-  )
-};
+const img = [
+    {
+        id:1,
+        img: require("../../assets/car.jpg")
+    },
+    {
+        id:2,
+        img: require("../../assets/img2.jpg")
+    },
+    {
+        id:3,
+        img: require("../../assets/img3.jpg")
+    },
+    {
+        id:4,
+        img: require("../../assets/img4.jpg")
+    },
+    {
+        id:5,
+        img: require("../../assets/img5.jpg")
+    },
+    
+]
+
+// const data = [
+//     {
+//         id_jour : 1,
+//         img_jour : "https://unsplash.com/photos/4HaWpdSoLd0",
+//         jour:'Lundi',
+//         stories:[
+//             {
+//                 story_id:1,
+//                 story_image:"https://unsplash.com/photos/4HaWpdSoLd0",
+//                 swipeText:'Custom swipe for the story',
+//                 onPress :()=> console.log("Story 1 swiped"),
+//             },
+//             {
+//                 story_id:2,
+//                 story_image:"https://unsplash.com/photos/4HaWpdSoLd0",
+//             },
+            
+//         ]
+        
+//     },
+//     {
+//         id_jour : 2,
+//         img_jour : "https://unsplash.com/photos/1m--lXnN1Bs",
+//         jour:'Mardi',
+//         stories:[
+//             {
+//                 story_id:1,
+//                 story_image:"https://unsplash.com/photos/1m--lXnN1Bs",
+//                 swipeText:'',
+//                 onPress :()=> console.log("Story 1 swiped"),
+//             },
+//             {
+//                 story_id:2,
+//                 story_image:"https://unsplash.com/photos/1m--lXnN1Bs",
+//             },
+            
+//         ]
+        
+//     },
+//     {
+//         id_jour : 3,
+//         img_jour : "https://unsplash.com/photos/tJiWEZjM7Eo",
+//         jour:'Mercredi',
+//         stories:[
+//             {
+//                 story_id:1,
+//                 story_image:"https://unsplash.com/photos/tJiWEZjM7Eo",
+//                 swipeText:'',
+//                 onPress :()=> console.log("Story 1 swiped"),
+//             },
+//             {
+//                 story_id:2,
+//                 story_image:"https://unsplash.com/photos/tJiWEZjM7Eo",
+//             },
+            
+//         ]
+        
+//     },
+//     {
+//         id_jour : 4,
+//         img_jour : "https://unsplash.com/photos/KiQ6wCEDhbs",
+//         jour:'Jeudi',
+//         stories:[
+//             {
+//                 story_id:1,
+//                 story_image:"https://unsplash.com/photos/KiQ6wCEDhbs",
+//                 swipeText:'',
+//                 onPress :()=> console.log("Story 1 swiped"),
+//             },
+//             {
+//                 story_id:2,
+//                 story_image:"https://unsplash.com/photos/KiQ6wCEDhbs",
+//             },
+            
+//         ]
+        
+//     },
+//     {
+//         id_jour : 5,
+//         img_jour : "https://unsplash.com/photos/pCsNcKMd3eM",
+//         jour:'Vendredi',
+//         stories:[
+//             {
+//                 story_id:1,
+//                 story_image:"https://unsplash.com/photos/pCsNcKMd3eM",
+//                 swipeText:'',
+//                 onPress :()=> console.log("Story 1 swiped"),
+//             },
+//             {
+//                 story_id:2,
+//                 story_image:"https://unsplash.com/photos/pCsNcKMd3eM",
+//             },
+            
+//         ]
+        
+//     },
+ 
+// ]
+
+const data = [
+    {
+        id: 1,
+        user_image: 'https://img.freepik.com/photos-gratuite/portrait-menuisier-professionnel-age-moyen-planche-bois-outils-debout-dans-son-atelier-menuiserie_342744-822.jpg?w=740&t=st=1670191494~exp=1670192094~hmac=65ff776a3566f168cc99d72dffbfc2bc7343bddde6665e559dd61a7ffd33036d',
+        user_name: "Lundi",
+        stories: [
+            {
+                story_id: 1,
+                story_image: "https://image.freepik.com/free-vector/universe-mobile-wallpaper-with-planets_79603-600.jpg",
+                swipeText:'Custom swipe text for this story',
+                onPress: () => console.log('story 1 swiped'),
+            },
+            {
+                story_id: 2,
+                story_image: "https://image.freepik.com/free-vector/mobile-wallpaper-with-fluid-shapes_79603-601.jpg",
+            }]
+    },
+    {
+        id: 2,
+        user_image: 'https://images.unsplash.com/photo-1511367461989-f85a21fda167?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80',
+        user_name: "Mardi",
+        stories: [
+            {
+                story_id: 1,
+                story_image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjORKvjcbMRGYPR3QIs3MofoWkD4wHzRd_eg&usqp=CAU",
+                swipeText:'Custom swipe text for this story',
+                onPress: () => console.log('story 1 swiped'),
+            },
+            {
+                story_id: 2,
+                story_image: "https://files.oyebesmartest.com/uploads/preview/vivo-u20-mobile-wallpaper-full-hd-(1)qm6qyz9v60.jpg",
+                swipeText:'Custom swipe text for this story',
+                onPress: () => console.log('story 2 swiped'),
+            }]
+    },
+    {
+        id: 3,
+        user_image: 'https://images.unsplash.com/photo-1511367461989-f85a21fda167?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80',
+        user_name: "Mercredi",
+        stories: [
+            {
+                story_id: 1,
+                story_image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjORKvjcbMRGYPR3QIs3MofoWkD4wHzRd_eg&usqp=CAU",
+                swipeText:'Custom swipe text for this story',
+                onPress: () => console.log('story 1 swiped'),
+            },
+            {
+                story_id: 2,
+                story_image: "https://files.oyebesmartest.com/uploads/preview/vivo-u20-mobile-wallpaper-full-hd-(1)qm6qyz9v60.jpg",
+                swipeText:'Custom swipe text for this story',
+                onPress: () => console.log('story 2 swiped'),
+            }]
+    },
+    {
+        id: 4,
+        user_image: 'https://images.unsplash.com/photo-1511367461989-f85a21fda167?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80',
+        user_name: "Jeudi",
+        stories: [
+            {
+                story_id: 1,
+                story_image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjORKvjcbMRGYPR3QIs3MofoWkD4wHzRd_eg&usqp=CAU",
+                swipeText:'Custom swipe text for this story',
+                onPress: () => console.log('story 1 swiped'),
+            },
+            {
+                story_id: 2,
+                story_image: "https://files.oyebesmartest.com/uploads/preview/vivo-u20-mobile-wallpaper-full-hd-(1)qm6qyz9v60.jpg",
+                swipeText:'Custom swipe text for this story',
+                onPress: () => console.log('story 2 swiped'),
+            }]
+    },
+    {
+        id: 5,
+        user_image: 'https://images.unsplash.com/photo-1511367461989-f85a21fda167?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80',
+        user_name: "Vendredi",
+        stories: [
+            {
+                story_id: 1,
+                story_image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjORKvjcbMRGYPR3QIs3MofoWkD4wHzRd_eg&usqp=CAU",
+                swipeText:'Custom swipe text for this story',
+                onPress: () => console.log('story 1 swiped'),
+            },
+            {
+                story_id: 2,
+                story_image: "https://files.oyebesmartest.com/uploads/preview/vivo-u20-mobile-wallpaper-full-hd-(1)qm6qyz9v60.jpg",
+                swipeText:'Custom swipe text for this story',
+                onPress: () => console.log('story 2 swiped'),
+            }]
+    }
+
+];
+
+const Storylist = () =>{
+    return(
+            <InstaStory 
+            data={data}
+            duration={10}
+            onStart={item => console.log(item)}
+            onClose={item => console.log('close: ', item)}
+            customSwipeUpComponent={<View>
+                                <Text>Swipe</Text>
+                            </View>}
+            style={{marginTop: 30,}}
+            avatarSize={55}
+            />
+    )
+}
+
+// const Storylist = ()=> {
+//   return (
+//     <View style={styles.container}    
+//     >
+//         <View style={styles.storyContainer}>
+//                 {
+//                     stories.map((story, index) =>{
+//                         return(
+//                             <TouchableOpacity>
+//                                 <View style={styles.circleContainer} >
+//                                     <ProfileHead key={story.id} uri={story.uri} />
+//                                     <Text>{story.jour}</Text>
+//                                 </View>
+//                             </TouchableOpacity>
+//                         );
+//                     })
+//                 }
+//         </View>
+//     </View>
+//   )
+// };
 
 const ProfileHead = ({uri}) =>{
     return(
@@ -96,4 +328,4 @@ const styles = StyleSheet.create({
     }
 })
 
-export default Storylist
+export default Storylist;
